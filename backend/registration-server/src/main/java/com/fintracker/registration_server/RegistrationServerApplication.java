cd6a6fbb4e119e736cdaaae14807b710f0855bfd
KLINY1337 <semyon.chernomurov@gmail.com>
2024-02-17 13:18:18 +0300

    initialized registration-server

---
+++ backend/registration-server/src/main/java/com/fintracker/registration_server/RegistrationServerApplication.java
@@ -0,0 +1,13 @@
+package com.fintracker.registration_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationServerApplication.class, args);
+	}
+
+}
