628d4e63278ff7a62de4d22c16226c89c50d701e
Alex Smolnikov <alesha.smolnikoff@gmail.com>
2024-02-29 13:38:07 +0300

    spring init

---
+++ backend/fintracker-monolith/src/main/java/com/fintracker/backend/fintrackermonolith/FintrackerMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.fintracker.backend.fintrackermonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackerMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintrackerMonolithApplication.class, args);
+	}
+
+}
