705d1348bbcc9b2ff195c5a67027e557a2a17399
Alex Smolnikov <alesha.smolnikoff@gmail.com>
2024-05-08 14:00:18 +0300

    Demo CORS Mappings

--- backend/fintracker-monolith/src/main/java/com/fintracker/backend/fintrackermonolith/FintrackerMonolithApplication.java
+++ backend/fintracker-monolith/src/main/java/com/fintracker/backend/fintrackermonolith/FintrackerMonolithApplication.java
@@ -3,7 +3,10 @@ package com.fintracker.backend.fintrackermonolith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableCaching
@@ -12,6 +15,17 @@ public class FintrackerMonolithApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FintrackerMonolithApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET","POST","PUT","OPTIONS","DELETE");
+				System.out.println(1);
+			}
+		};
 	}
 
 }
