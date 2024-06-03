bae0b82732712311c0ee5012366bdaaa67e8997a
KLINY1337 <semyon.chernomurov@gmail.com>
2024-06-03 20:24:41 +0300

    2222

--- frontend/src/enviroments/enviroment.ts
+++ frontend/src/enviroments/enviroment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
   secure: false,
-  auth: 'http://89.223.121.170:8080/auth',
-  api: 'http://89.223.121.170:8080/api/v1',
-  investment_positions: 'http://89.223.121.170:8080/api/v1/investment_positions',
-  portfolios: 'http://89.223.121.170:8080/api/v1/portfolios',
-  gigachat: 'http://89.223.121.170:8080/gigachat',
+  auth: 'http://5.23.48.222:8080/auth',
+  api: 'http://5.23.48.222:8080/api/v1',
+  investment_positions: 'http://5.23.48.222:8080/api/v1/investment_positions',
+  portfolios: 'http://5.23.48.222:8080/api/v1/portfolios',
+  gigachat: 'http://5.23.48.222:8080/gigachat',
 };
